f24b76aa200bb04600668facf462a48af55adde9
Dell <Dell@10.15.59.147>
2018-11-09 21:58:03 -0200

    Merge origin/master
    Conflicts:
    	src/br/udesc/ceavi/estude/model/Conteudo.java

--- src/br/udesc/ceavi/estude/model/Privilegio.java
+++ src/br/udesc/ceavi/estude/model/Privilegio.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 /**
@@ -28,6 +29,10 @@ public class Privilegio {
     
     @ManyToOne
     private Tela tela;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
 
     public Privilegio() {
         super();
