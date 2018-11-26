2d2cd42d3cf5fbbc2440fe8efae3dc0b4c204507
Dell <Dell@DESKTOP-0C1NADK>
2018-11-25 22:45:53 -0200

    Merge origin/master
    Conflicts:
    	src/br/udesc/ceavi/estude/model/Conteudo.java

--- src/br/udesc/ceavi/estude/view/listener/MenuActionListener.java
+++ src/br/udesc/ceavi/estude/view/listener/MenuActionListener.java
@@ -7,10 +7,12 @@ package br.udesc.ceavi.estude.view.listener;
 
 import br.udesc.ceavi.estude.model.Acao;
 import br.udesc.ceavi.estude.model.Conteudo;
+import br.udesc.ceavi.estude.model.Disciplina;
 import br.udesc.ceavi.estude.model.Usuario;
 import br.udesc.ceavi.estude.view.frame.FrameCRUD;
 import br.udesc.ceavi.estude.view.frame.FrameCRUDAcao;
 import br.udesc.ceavi.estude.view.frame.FrameCRUDConteudo;
+import br.udesc.ceavi.estude.view.frame.FrameCRUDDisciplina;
 import br.udesc.ceavi.estude.view.frame.FrameCRUDUsuario;
 import br.udesc.ceavi.estude.view.principal.FrameSistema;
 import java.awt.event.ActionEvent;
@@ -20,16 +22,18 @@ import java.util.logging.Logger;
 
 /**
  * Classe que define os atributos e comportamento dos ActionListeners dos Menus
+ *
  * @author Gerson Luiz
  * @since 05/10/2018
  */
-public class MenuActionListener implements ActionListener{
+public class MenuActionListener implements ActionListener {
+
     //Classe que definira a especialização do FrameCRUD
     private final Class<? extends FrameCRUD> classe;
-    
+
     //JInternalFrame que deverá ser cosntruída
     private FrameCRUD frame;
- 
+
     //Parent (tela da aplicação principal) onde as JInternalFrame deverão ser exibidas
     private final FrameSistema tela;
 
@@ -38,31 +42,31 @@ public class MenuActionListener implements ActionListener{
         this.classe = classe;
     }
 
-        
     @Override
     public void actionPerformed(ActionEvent e) {
         try {
             //Definindo o model do frame
             frame = classe.newInstance();
-            
+
             // Definindo o internalFrame da tela
             tela.adicionarFrameInterno(frame);
-            
-            
+
             //Define o Controller para um cadastro CRUDEndereco
-            
             //
-            if( frame.getClass() == FrameCRUDUsuario.class ){
-                ListenersCRUDUsuario listenerUsuario = new ListenersCRUDUsuario( new Usuario(), frame);
-            } else if( frame.getClass() == FrameCRUDAcao.class){
-                ListenersCRUDAcao listenerConteudo = new ListenersCRUDAcao( new Acao(), frame);
-            } else if( frame.getClass() == FrameCRUDConteudo.class){
-                ListenersCRUDConteudo listenerConteudo = new ListenersCRUDConteudo( new Conteudo(), frame);
-            }else {
-            }{
+            if (frame.getClass() == FrameCRUDUsuario.class) {
+                ListenersCRUDUsuario listenerUsuario = new ListenersCRUDUsuario(new Usuario(), frame);
+            } else if (frame.getClass() == FrameCRUDAcao.class) {
+                ListenersCRUDAcao listenerConteudo = new ListenersCRUDAcao(new Acao(), frame);
+            } else if (frame.getClass() == FrameCRUDConteudo.class) {
+                ListenersCRUDConteudo listenerConteudo = new ListenersCRUDConteudo(new Conteudo(), frame);
+            } else if (frame.getClass() == FrameCRUDDisciplina.class) {
+                ListenersCRUDDisciplina listenerDisciplina = new ListenersCRUDDisciplina(new Disciplina(), frame);
+            } else {
+            }
+            {
             }
             frame.setVisible(true);
-            
+
         } catch (InstantiationException | IllegalAccessException ex) {
             Logger.getLogger(MenuActionListener.class.getName()).log(Level.SEVERE, null, ex);
         }
